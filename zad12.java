558072446d24fd0a907afe96f3666540a7f97ce4
Jungso23 <ktabor2310@gmail.com>
2025-01-17 19:23:12 +0100

    Create zad12.java

---
+++ zad12.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GraZaDuzaZaMala {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int wylosowanaLiczba = random.nextInt(100) + 1;  // Losujemy liczbę z zakresu 1...100
+        Scanner scanner = new Scanner(System.in);
+        int podanaLiczba = 0;
+
+        System.out.println("Witaj w grze 'Za dużo, za mało'! Spróbuj zgadnąć liczbę.");
+
+        // Pętla do momentu, gdy użytkownik zgadnie liczbę
+        while (podanaLiczba != wylosowanaLiczba) {
+            System.out.print("Podaj liczbę (1-100): ");
+            podanaLiczba = scanner.nextInt();
+
+            if (podanaLiczba > wylosowanaLiczba) {
+                System.out.println("Podałeś za dużą wartość.");
+            } else if (podanaLiczba < wylosowanaLiczba) {
+                System.out.println("Podałeś za małą wartość.");
+            }
+        }
+
+        System.out.println("Gratulacje! Zgadłeś liczbę.");
+    }
+}
